8d8690c72a4bbcfced7ea6d4978a53e3c957e33d
Daniel Pantoja <danielpantojab13@gmail.com>
2026-04-04 22:43:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const glow = document.querySelector('.cursor-glow');
+
+document.addEventListener('mousemove', (e) => {
+  glow.style.left = e.clientX + 'px';
+  glow.style.top = e.clientY + 'px';
+});
